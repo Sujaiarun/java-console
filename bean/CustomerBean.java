df54925ff7492946277ec0fcceb2baf0dff64ec2
Sujaiarun <110587329+Sujaiarun@users.noreply.github.com>
2025-02-28 14:49:17 +0530

    Add files via upload

---
+++ bean/CustomerBean.java
@@ -0,0 +1,130 @@
+package com.obs.bean;
+
+public class CustomerBean {
+	private int cACno;
+	private String cname;
+	private int cbal;
+	private String cmail;
+	private String cpass;
+	private String cmob;
+	private String cadd;
+	
+	
+	public CustomerBean() {
+		super();
+	}
+
+	public CustomerBean(int cACno, String cname, int cbal, String cmail, String cpass, String cmob, String cadd) {
+		super();
+		this.cACno = cACno;
+		this.cname = cname;
+		this.cbal=cbal;
+		this.cmail = cmail;
+		this.cpass = cpass;
+		this.cmob = cmob;
+		this.cadd = cadd;
+	}
+
+	public int getcACno() {
+		return cACno;
+	}
+
+	public void setcACno(int cACno) {
+		this.cACno = cACno;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+	public int getCbal() {
+		return cbal;
+	}
+
+	public void setCbal(int cbal) {
+		this.cbal = cbal;
+	}
+
+	public String getCmail() {
+		return cmail;
+	}
+
+	public void setCmail(String cmail) {
+		this.cmail = cmail;
+	}
+
+	public String getCpass() {
+		return cpass;
+	}
+
+	public void setCpass(String cpass) {
+		this.cpass = cpass;
+	}
+
+	public String getCmob() {
+		return cmob;
+	}
+
+	public void setCmob(String cmob) {
+		this.cmob = cmob;
+	}
+
+	public String getCadd() {
+		return cadd;
+	}
+
+	public void setCadd(String cadd) {
+		this.cadd = cadd;
+	}
+
+	@Override
+	public String toString() {
+		return "CustomerBean [cACno=" + cACno + ", cname=" + cname + ", cbal=" + cbal + ", cmail=" + cmail + ", cpass="
+				+ cpass + ", cmob=" + cmob + ", cadd=" + cadd + "]";
+	}
+
+
+	
+}
+172.16.0.211
+255.255.248.0
+172.16.3.1
+8.8.8.8
+4.4.2.2
+Day 1 Topics
+Quants
+Percentages 
+Profit and Loss 
+Simple Interest and Compound Interest 
+
+Reasoning
+Number Series 
+Letter Series 
+Odd man Out 
+Coding and Decoding
+ Day 2 Topics 
+Quants
+Ratio and Proportion 
+Problems on Ages
+Partnership 
+Allegation and Mixtures 
+
+Reasoning
+Blood Relations
+Direction Sense Test
+Seating Arrangements
+Day 3 topics
+Quants
+Time, Speed and Distance 
+Trains
+Boats and Streams 
+
+Reasoning
+Clocks
+Calendars
+Mirror images 
+Water images
